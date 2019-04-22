438d3ddab268e3c0832613536eb2c1812eb10805
muralidhar <immurali5@gmail.com>
2019-04-22 15:37:03 +0530

    oops

---
+++ loops/loops.py
@@ -0,0 +1,12 @@
+movie=input("enter the pavan kalyan movie name")
+if movie == "janatha garage":
+    print("this is the all time recorded movie in ap")
+elif movie == "maharshi":
+    print("this is the all time recorded movie in ap")
+elif movie == "mirchi":
+    print("this is the all time recorded movie in ap")
+elif movie == "gabbar singh":
+    print("this is the all time recorded movie in ap")
+else:
+    print("trivikram is the one of great director in india")
+
