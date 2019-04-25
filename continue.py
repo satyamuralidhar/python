055f6348e5642204199665db0c0ab5f8910eb580
muralidhar <38804803+immurali5@users.noreply.github.com>
2019-04-25 10:48:58 +0530

    continue and break statement

---
+++ continue.py
@@ -0,0 +1,21 @@
+#Break
+
+numbers = [1, 2, 3, 4, 5]
+
+for num in numbers:
+    if num == 4:
+        print('hey got it satya!')
+        break
+    print(num)
+
+
+
+#Continue
+
+amanora = [7, 8, 9, 10, 11]
+
+for sam in amanora:
+    if sam == 8:
+        print('hey i got it murali !')
+        continue
+    print(sam)
