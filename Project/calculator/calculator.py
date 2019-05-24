00b437125268fa6905784815aebd3058379ef6e2
muralidhar <38804803+immurali5@users.noreply.github.com>
2019-05-24 13:01:31 +0530

    Update calculator.py

--- Project/calculator/calculator.py
+++ Project/calculator/calculator.py
@@ -8,18 +8,18 @@ def multiply(num1 , num2):
     return num1*num2
 def divide(num1 , num2):
     return num1/num2
-caliculator = input("select from 1, 2, 3, 4 -:  " )
+calculator = input("select from 1, 2, 3, 4 -:  " )
 
 first = int(input("enter  first number: "))
 second = int(input("enter second number: "))
 
-if caliculator == "1":
+if calculator == "1":
     print(add(first , second))
-elif caliculator == "2":
+elif calculator == "2":
     print(subtract(first , second))
-elif caliculator == "3":
+elif calculator == "3":
     print(multiply(first, second))
-elif caliculator == "4":
+elif calculator == "4":
     print(divide(first, second))
 else:
     print("error it's not possible please check select from")
