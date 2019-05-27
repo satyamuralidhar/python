6e0029bbe3dafc0be7aa122deddd28b00644c097
muralidhar <38804803+immurali5@users.noreply.github.com>
2019-05-27 15:15:41 +0530

    Create inheritance.py

---
+++ oops/inheritance.py
@@ -0,0 +1,48 @@
+class A():
+    def MyData(self):
+        print('this is mydata')
+class B(A):
+    def MyData2(self):
+        self.MyData()
+        print('this is mydata2')
+class C():
+    def MyData3(self):
+        print('this is mydata3')
+x = A()
+y = B()
+z = C()
+x.MyData()
+y.MyData2()
+
+#output:
+#this is mydata
+#this is mydata
+#this is mydata2
+
+
+##################
+
+class A():
+    def Ram(self):
+        print("hi this is ram")
+class B():
+    def Sam(self):
+        print("hi this is sam")
+class C(A):
+    def Lakshman(self):
+        print("hi this is lakshman")
+x = A()
+y = B()
+z = C()
+x.Ram()
+y.Sam()
+z.Lakshman()
+z.Ram()
+
+
+
+#output:
+#hi this is ram
+#hi this is sam
+#hi this is lakshman
+#hi this is ram
