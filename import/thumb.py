490a4b906cd67e6b25c34e728e3ba5dde0e3246a
muralidhar <38804803+immurali5@users.noreply.github.com>
2019-05-29 10:47:42 +0530

    thumb

---
+++ import/thumb.py
@@ -0,0 +1,8 @@
+def add(numbers):
+    return numbers + 10
+def subtract(numbers):
+    return numbers - 5
+def multiply(numbers):
+    return numbers * 3
+def divide(numbers):
+    return numbers / 2
