c3be481d5fbc150e9b72400319fe8b4264bc85ac
muralidhar <38804803+immurali5@users.noreply.github.com>
2019-06-05 15:13:29 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -56,3 +56,37 @@ print(emp2.first)
 print(emp2.last)
 print(emp2.fullname)
 print(emp2.email)
+
+
+#Deleter Decorator
+
+class Employee:
+    def __init__(self , first , last):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last +'@gmail.com'
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first,self.last)
+    @fullname.setter
+    def fullname(self , name):
+        first , last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print("delete name!")
+        self.first = None
+        self.last = None
+
+emp = Employee("muralidhar" , "peddireddi")
+emp2 = Employee('ramareddy' , "gudla")
+emp.fullname = 'mohan peddireddi'
+print(emp.email)
+print(emp.fullname)
+print(emp2.first)
+print(emp2.last)
+print(emp2.fullname)
+print(emp2.email)
+del emp.fullname
