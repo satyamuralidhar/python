e8dc15451e72e4ffc77a857ae0e9b0c6805e1a49
satya <38804803+satyamuralidhar@users.noreply.github.com>
2019-10-25 14:46:31 +0530

    Update for_range.py

--- for_range.py
+++ for_range.py
@@ -4,3 +4,37 @@ for i in range(100,104):
         print(num,i)
         if num==102:
             pass
+
+        
+        
+        
+        
+        
+        
+#2.Create the below pattern using nested for loop in Python.
+        *  
+        * *  
+        * * *  
+        * * * *  
+        * * * * *  
+        * * * * 
+        * * * 
+        * *  
+        * 
+
+        
+number = int(input("enter the values: "))
+for i in range(1,number+1):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+for k in range(number-2,-1,-1):
+    for m in range(k,-1,-1):
+        print("*",end="")
+    print()
+        
+        
+        
+      
+ 
+ 
