fa790ed0200b6e182adb294e02e1f0c71344b731
satya <38804803+satyamuralidhar@users.noreply.github.com>
2020-03-11 15:33:33 +0530

    private encapsulation

---
+++ encap.py
@@ -0,0 +1,9 @@
+class Satya:
+  def __init__(self):
+    self.__updatesoftware()
+  def drive(self):
+    print("driving")
+  def __updatesoftware(self):
+    print("private")
+x = Satya()
+x.drive()
