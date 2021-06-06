c4e33f2cb27912051ffa2061c6971f941a5a165a
satya <38804803+satyamuralidhar@users.noreply.github.com>
2021-06-06 11:18:02 +0530

    Create star.py

---
+++ star.py
@@ -0,0 +1,12 @@
+#      * 
+#    * * *
+#   * * * *
+#  * * * * *
+# * * * * * *
+
+for i in range(0,5):
+    for j in range(0,5-i):
+        print(" ",end='')
+    for k in range(0,i+1):
+        print("*",end="")
+    print("")
